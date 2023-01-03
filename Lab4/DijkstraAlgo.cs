ac10ea0444d9d39efe2ca665b9def6049e0174fb
milka_deluxe <milagalko1@gmail.com>
2023-01-04 00:44:32 +0200

    Dijkstra finished

---
+++ Lab4/DijkstraAlgo.cs
@@ -0,0 +1,57 @@
+namespace Lab4;
+
+public class DijkstraAlgo
+{
+    private int _size;
+    private int[] _shortestPathes;
+    private bool[] _visitedNodes;
+    private int[,] _matrix;
+
+    private int NearestNodeIndex()
+    {
+        int pathLength = int.MaxValue;
+        int nearestNode = -1;
+        for (int node = 0; node < _size; node++)
+        {
+            if (!_visitedNodes[node] && _shortestPathes[node] < pathLength)
+            {
+                pathLength = _shortestPathes[node];
+                nearestNode = node;
+            }
+        }
+
+        return nearestNode;
+    }
+
+    private void UpdatePathesLength(int node)
+    {
+        for (int dest = 0; dest < _size; dest++)
+        {
+            int length = _matrix[node, dest];
+            if (_matrix[node, dest] != 0 &&
+                !_visitedNodes[dest] &&
+                length + _shortestPathes[node] < _shortestPathes[dest])
+            {
+                _shortestPathes[dest] = length + _shortestPathes[node];
+            }
+        }
+    }
+
+    public int[] Solve(int[,] matrix, int start)
+    {
+        _size = matrix.GetLength(0);
+        _visitedNodes = new bool[_size];
+        _shortestPathes = Enumerable.Repeat(int.MaxValue, _size).ToArray();
+        _shortestPathes[start] = 0;
+        _matrix = matrix;
+
+        int node;
+        while ((node = NearestNodeIndex()) != -1)
+        {
+            UpdatePathesLength(node);
+            _visitedNodes[node] = true;
+        }
+
+        return _shortestPathes;
+    }
+}
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Security.Cryptography;
+using Lab4;
 
-Console.WriteLine("Hello, World!");
+int[,] matrix =
+{
+    {0, 5, 0, 0, 8, 0},
+    {0, 0, 0, 1, 0, 0},
+    {0, 7, 0, 0, 0, 2},
+    {0, 0, 0, 0, 0, 9},
+    {0, 0, 4, 3, 0, 2},
+    {0, 0, 0, 0, 0, 0}
+};
+
+var dijkstra = new DijkstraAlgo();
+var results = dijkstra.Solve(matrix, 0);
+Console.WriteLine(string.Join(", ", results));
